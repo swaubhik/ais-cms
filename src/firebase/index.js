d3ab5bf0eafe1412fa08a650e3525085e84d2740
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-16 04:32:09 +0530

    Create index.js
    firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+//initialize firebase auth
+const auth = getAuth()
+
+// initialize firebase firestore
+const db = getFirestore()
+
+export { app, auth, db }
