f150d54da18a8bbac30c50a2acf5a149541bb870
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-15 23:10:16 +0530

    Update HomeView.vue
    UI Changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex items-center justify-center font-bold">
+    <div class="flex-1">
+      <h1 class="text-8xl text-dark">Easy Content Creation</h1>
+      <p class="mt-8 text-2xl text-dark">Create content for AIS - AI for Study</p>
+      <div class="mt-8 flex">
+        <router-link
+          class="mr-4 rounded-lg bg-dark px-4 py-2 text-xl text-light shadow-xl hover:bg-dark-lighter"
+          :to="{ name: 'login' }"
+          >Get Started</router-link
+        >
+      </div>
+    </div>
+    <div class="flex">
+      <img class="max-w-2xl drop-shadow-xl" src="@/assets/images/hero-image.png" alt="hero-image" />
+    </div>
+  </div>
 </template>
+<script></script>
