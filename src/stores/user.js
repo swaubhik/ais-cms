471b2e02536ae8df94b2b4f07ab85d99fdc60173
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-16 04:32:54 +0530

    Create user.js
    user stores

---
+++ src/stores/user.js
@@ -0,0 +1,84 @@
+import { defineStore } from 'pinia'
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth'
+import { auth } from '../firebase'
+import router from '../router'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    userData: null,
+    loadingUser: false,
+    loadingSession: false,
+    errorServer: null
+  }),
+  actions: {
+    setErrorServer(error) {
+      this.errorServer = error
+    },
+    async registerUser(email, password) {
+      this.loadingUser = true
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        this.userData = userCredential.user
+        console.log({
+          email: this.userData.email,
+          uid: this.userData.uid,
+          photoURL: this.userData.photoURL
+        })
+        router.push({ name: 'login' })
+      } catch (error) {
+        console.log(error)
+        this.errorServer = error.message
+      } finally {
+        this.loadingUser = false
+      }
+    },
+    async loginUser(email, password) {
+      this.loadingUser = true
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        this.userData = userCredential.user
+        router.push({ name: 'dashboard' })
+      } catch (error) {
+        console.log(error)
+        this.errorServer = error.message
+      } finally {
+        this.loadingUser = false
+      }
+    },
+    async logoutUser() {
+      try {
+        await signOut(auth)
+        this.userData = null
+        router.push({ name: 'login' })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    currentUser() {
+      return new Promise((resolve, reject) => {
+        const unsuscribe = onAuthStateChanged(
+          auth,
+          (user) => {
+            if (user) {
+              this.userData = {
+                email: user.email,
+                uid: user.uid,
+                photoURL: user.photoURL
+              }
+            } else {
+              this.userData = null
+            }
+            resolve(user)
+          },
+          (e) => reject(e)
+        )
+        unsuscribe()
+      })
+    }
+  }
+})
