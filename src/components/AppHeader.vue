04738d8a6a11353fc2a652c036f7c90ce2f571aa
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-15 23:09:13 +0530

    Update AppHeader.vue
    added conditional navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,16 @@
 <template>
-  <header class="bg-light text-lg font-bold text-dark">
+  <header class="bg-light py-2 text-lg font-bold text-dark">
     <div class="mx-auto flex max-w-8xl items-center justify-between">
-      <img alt="App Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <RouterLink :to="{ name: 'home' }"
+        ><img alt="App Logo" class="logo" src="@/assets/logo.svg" width="125" height="125"
+      /></RouterLink>
       <div class="flex">
         <nav class="flex items-center justify-center gap-12">
-          <RouterLink to="/">Log In</RouterLink>
-          <RouterLink class="rounded-lg bg-dark px-4 py-2 text-light" to="/about"
+          <RouterLink v-show="activeRoute !== 'login'" :to="{ name: 'login' }">Log In</RouterLink>
+          <span v-show="activeRoute === 'login'">Don't have an account</span>
+          <RouterLink
+            :to="{ name: 'register' }"
+            class="rounded-lg bg-dark px-4 py-2 text-light hover:bg-dark-light"
             >Create an account</RouterLink
           >
         </nav>
@@ -15,7 +20,19 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      activeRoute: null
+    }
+  },
+  watch: {
+    $route(to) {
+      this.activeRoute = to.name
+      console.log(this.activeRoute)
+    }
+  }
+}
 </script>
 
 <style></style>
