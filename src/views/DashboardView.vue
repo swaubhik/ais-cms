e4596723e2453d4c89c1cb83059665db52dce66d
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-16 04:33:06 +0530

    Create DashboardView.vue
    avatar api

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex min-h-80vh items-center justify-center">
+    <h1 class="text-4xl font-bold">Dashboard</h1>
+    <img
+      :src="
+        'https://api.dicebear.com/6.x/avataaars/svg?seed=' +
+        uid +
+        '&background=%23fff&radius=50&margin=10&size=64'
+      "
+      alt="Avatar"
+    />
+  </div>
+</template>
+
+<script>
+import { useUserStore } from '../stores/user'
+export default {
+  data() {
+    return {
+      userStore: useUserStore(),
+      uid: useUserStore().userData.uid
+    }
+  },
+  mounted() {
+    this.userStore.userData
+  }
+}
+</script>
+
+<style></style>
