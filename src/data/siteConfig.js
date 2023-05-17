9fa7b63422db0bfc6060058ee60c40005a1dda31
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-17 23:23:11 +0530

    siteConfig.js

---
+++ src/data/siteConfig.js
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  siteTitle: 'Gatsby Starter Personal Website', // Site title.
+  siteTitleShort: 'Gatsby Starter', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'GatsbyJS Starter Personal Website', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://gatsby-starter-personal-website.greglobinski.com', // Domain of your website without pathPrefix.
+  siteDescription: 'Playful & Colorful One-Page website with Parallax effect' // Website description used for RSS feeds/meta description tag.
+}
