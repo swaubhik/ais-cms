1977d52a726864fa7e650bd41a00c601aab5319b
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-19 02:42:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#F6D65D',
-      light: '#FDF7DF',
-      dark: '#0B2435',
-      'dark-light': '#20435C',
-      'dark-lighter': '#355C76',
-      error: '#FF0000'
-    },
     extend: {
+      colors: {
+        primary: '#F6D65D',
+        light: '#FDF7DF',
+        dark: '#0B2435',
+        'dark-light': '#20435C',
+        'dark-lighter': '#355C76',
+        error: '#FF0000'
+      },
       backgroundImage: {
         'hero-pattern': "url('/src/assets/images/bg-pattern.png')"
       },
