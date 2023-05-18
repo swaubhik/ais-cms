4a4cbad659e0dc55111676363a237e220992c24f
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-19 02:41:40 +0530

    Create AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2 class="absolute bottom-0 left-1/2 transform -translate-x-1/2">
+    &lt;/&gt; with 💜 by
+    <a class="font-bold" href="https://swaubhik.github.io/old-swaubhik.github.io">swaubhik</a>
+  </h2>
+</template>
