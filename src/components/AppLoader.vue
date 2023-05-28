83849b7492317e66a933d68c164166705483f06f
anowar24 <anowarhu024@gmail.com>
2023-05-28 14:18:27 +0530

    loader size change

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="absolute top-0 left-0 bg-primary/90 h-full w-full z-50">
     <div class="flex items-center justify-center h-full">
-      <img class="h-1/2" src="/loader.gif" alt="loader" />
+      <img class="h-1/4" src="/loader.gif" alt="loader" />
     </div>
   </div>
 </template>
